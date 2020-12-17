e401730444b844a8b48c678bf98332695db17337
David Kucher <mrdkucher@gmail.com>
2020-12-16 21:05:01 -0600

    Initial commit

---
+++ kucher_resume/main.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+
+@bp.route("/")
+def index():
+    return render_template('main/index.html')
+
+
+@bp.route("/about")
+def about():
+    return render_template('main/about.html')
+
+
+@bp.route("/projects")
+def projects():
+    return render_template('main/projects.html')
